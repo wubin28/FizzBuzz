6dfb717ce1ffc68bd794f835f01877b4b74a2965
Ben Wu <wubin28@gmail.com>
2013-07-05 21:12:13 +0800

    Delegated sound making requests to subclasses of SoundMaker in the fixture class.

--- fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/fixture/打印FizzBuzz后从1到100.java
+++ fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/fixture/打印FizzBuzz后从1到100.java
@@ -1,5 +1,9 @@
 package com.wubinben.kata.fizzbuzz.fixture;
 
+import com.wubinben.kata.fizzbuzz.Buzz;
+import com.wubinben.kata.fizzbuzz.Fizz;
+import com.wubinben.kata.fizzbuzz.SoundMaker;
+
 /**
  * User: Ben
  * Date: 13-7-5
@@ -8,12 +12,24 @@ package com.wubinben.kata.fizzbuzz.fixture;
 public class 打印FizzBuzz后从1到100 {
 
   private int number;
+  private SoundMaker fizz;
+  private SoundMaker buzz;
+
+  public 打印FizzBuzz后从1到100() {
+    this.buzz = Buzz.newInstance(null, "buzz");
+    this.fizz = Fizz.newInstance(buzz, "fizz");
+  }
 
   public void set从1到100(int number) {
     this.number = number;
   }
   
   public String FizzBuzz后的数() {
+    StringBuffer sound = new StringBuffer("");
+    this.fizz.handleRequest(this.number, sound);
+    if (sound.toString().equals("")) {
+      sound.append(Integer.toString(this.number));
+    }
     if (number % 3 == 0 && number % 5 == 0) {
       return "FizzBuzz";
     }
